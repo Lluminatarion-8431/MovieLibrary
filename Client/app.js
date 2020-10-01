10a42a8439e2eae40329ed9948a9d91ac77eb89b
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2020-10-01 07:36:26 -0700

    Added function method of processForm and getMovieById for type get and post.

--- Client/app.js
+++ Client/app.js
@@ -1,27 +1,64 @@
-(function($){
-    function processForm( e ){
-        var dict = {
-        	Title : this["title"].value,
-            Director: this["director"].value,
-            Genre: this["genre"].value
-        };
+
+
+
+
+
+
+
+function processForm( e ){
+    var data = {
+        Title : this["title"].value,
+        Director: this["director"].value,
+        Genre: this["genre"].value
+    };
+
+    if(data.Title == "" || data.Director == ""){
+        alert("Please enter a movie and try again");
+        processForm();
+    }
+    else{
 
         $.ajax({
             url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
+            dataType: 'text',
             type: 'post',
             contentType: 'application/json',
-            data: JSON.stringify(dict),
+            data: JSON.stringify(data),
             success: function( data, textStatus, jQxhr ){
                 $('#response pre').html( data );
+                
             },
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
             }
+        
+        }).then(function(){
+            createTable();
         });
 
+        document.getElementById('my-form').reset();
         e.preventDefault();
     }
+}
 
-    $('#my-form').submit( processForm );
-})(jQuery);
+$('#my-form').submit( processForm );
+
+
+function getMovieById(){
+    var id = $('#movieId').val();
+    var movie;
+    $.ajax({
+        url: 'https://localhost:44325/api/movie/' + id,
+        dataType: 'json',
+        type: 'get',
+        contentType: 'application/json',
+        success: function( data, textStatus, jQxhr ){
+            movie = data;
+            console.log(movie);
+            changeInput(movie);           
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
+}
